8787b4533d2113ba46758d6a2dadc9b26f94fbbd
Jeff Ching <ching.jeff@gmail.com>
2015-08-21 16:17:00 -0700

    add field to return all ips

--- lib/action_dispatch/untrusted_ip.rb
+++ lib/action_dispatch/untrusted_ip.rb
@@ -4,13 +4,16 @@ require "action_dispatch/untrusted_ip/version"
 module ActionDispatch
   module UntrustedIp
     def untrusted_ip
-      untrusted_ips = split_ip_addresses(@env['HTTP_X_ORIG_FORWARDED_FOR'])
-      untrusted_ips = reject_trusted_ip_addresses(untrusted_ips)
-
-      return untrusted_ips.first if untrusted_ips.any?
+      untrusted_ips.tap do |ips|
+        return ips.first if ips.any?
+      end
 
       # fallback to the default ip
       ip
+    end
+
+    def untrusted_ips
+      reject_trusted_ip_addresses(split_ip_addresses(@env['HTTP_X_ORIG_FORWARDED_FOR']))
     end
   end
 end
